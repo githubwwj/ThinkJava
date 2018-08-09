fc4b3bb337538193d3448e971bba662bd593fef1
githubwwj <githubwwj@outlook.com>
2018-08-09 08:03:15 +0800

    新增对象引用

---
+++ ObjectReference.java
@@ -0,0 +1,12 @@
+class ObjectReference //对象引用  类的名字和文字名字是一致的，否则会出错的
+{
+	public static void main(String[] args) //程序入口
+	{
+		String str=new String("今天天不错，因为天气很晴朗!");
+		//通过new 关键字创建了一个对象
+		//RAM随机访问寄存器Random Access Memory(内存条),存储在内存的堆区中
+		//对象的引用存储在堆栈中（栈）
+		System.out.println(str);
+		
+	}
+}
